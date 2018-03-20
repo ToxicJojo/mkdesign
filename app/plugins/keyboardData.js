2e0c5457003fe866237b5618fd701feb76bfb07c
Johannes Thiel <Johannes.Thiel@hhu.de>
2018-03-20 13:33:56 +0100

    Added a 40% keyboard

--- app/plugins/keyboardData.js
+++ app/plugins/keyboardData.js
@@ -1,10 +1,11 @@
 import keyboard75percent from '../data/keyboard-75-percent.json'
 import keyboard60percent from '../data/keyboard-60-percent.json'
+import keyboard40percent from '../data/keyboard-40-percent.json'
 
 const keyboardDataPlugin = {
   install (Vue) {
     
-    const keyboards = [keyboard60percent, keyboard75percent, keyboard75percent]
+    const keyboards = [keyboard40percent, keyboard60percent, keyboard75percent]
 
     Vue.prototype.$keyboards = keyboards
   }
