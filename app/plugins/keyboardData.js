8106728ed8fd9de6ed9ccaa8becba251472b13f6
Johannes Thiel <Johannes.Thiel@hhu.de>
2018-03-20 13:39:26 +0100

    Renamed 75% to tenkeyless

--- app/plugins/keyboardData.js
+++ app/plugins/keyboardData.js
@@ -1,11 +1,11 @@
-import keyboard75percent from '../data/keyboard-75-percent.json'
+import keyboardTenkeyless from '../data/keyboard-tenkeyless.json'
 import keyboard60percent from '../data/keyboard-60-percent.json'
 import keyboard40percent from '../data/keyboard-40-percent.json'
 
 const keyboardDataPlugin = {
   install (Vue) {
     
-    const keyboards = [keyboard40percent, keyboard60percent, keyboard75percent]
+    const keyboards = [keyboard40percent, keyboard60percent, keyboardTenkeyless]
 
     Vue.prototype.$keyboards = keyboards
   }
