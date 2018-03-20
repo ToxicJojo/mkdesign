36072931e9100ae14b2fcc6d370058c5cc41bd34
Johannes Thiel <Johannes.Thiel@hhu.de>
2018-03-20 15:21:12 +0100

    Added a button to download the json of the keyboard

--- app/components/layout/Navbar.vue
+++ app/components/layout/Navbar.vue
@@ -9,6 +9,11 @@
     .navbar-menu
       .navbar-start
       .navbar-end
+        .navbar-item.has-dropdown.is-hoverable
+          a.navbar-link  Download
+            b-icon(icon='download')
+          .navbar-dropdown
+            a.navbar-item(@click='downloadJSON') JSON
         a.navbar-item(href='#/editor/layout' @click='$store.commit("editor/reset")')
           | Reset
           b-icon(icon='reload')
@@ -20,6 +25,16 @@ export default {
   data () {
     return {
     }
+  },
+  methods: {
+    downloadJSON () {
+      const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.$store.state.editor.currentKeyboard));
+      const downloadAnchorNode = document.createElement('a');
+      downloadAnchorNode.setAttribute("href",     dataStr);
+      downloadAnchorNode.setAttribute("download","keyboard.json");
+      downloadAnchorNode.click();
+      downloadAnchorNode.remove();
+    },
   },
 }
 </script>
