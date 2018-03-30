40cb1b19b0e3375725bf23d5df304e33a4426843
Johannes Thiel <Johannes.Thiel@hhu.de>
2018-03-30 17:47:48 +0200

    Adedd full layout keyboard

--- app/plugins/keyboardData.js
+++ app/plugins/keyboardData.js
@@ -1,10 +1,11 @@
 import keyboard40percent from '../data/keyboard-40-percent.json'
 import keyboard60percent from '../data/keyboard-60-percent.json'
 import keyboardTenkeyless from '../data/keyboard-tenkeyless.json'
+import keyboardFull from '../data/keyboard-full.json'
 
 const keyboardDataPlugin = {
   install (Vue) {
-    const keyboards = [keyboard40percent, keyboard60percent, keyboardTenkeyless]
+    const keyboards = [keyboard40percent, keyboard60percent, keyboardTenkeyless, keyboardFull]
 
     Vue.prototype.$keyboards = keyboards
   }
